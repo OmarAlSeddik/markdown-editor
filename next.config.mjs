3b1327d82f6b8bb907d501ec666036c270f4b376
unknown <omaralseddik@gmail.com>
2023-06-03 04:18:12 +0300

    Ignore Build Errors

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,9 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
